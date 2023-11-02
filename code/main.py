490b07524e5dbffd8e0e3be5b861c982353d6e61
GrgoirMeunier <gregoire.meunier@polytechnique.edu>
2023-11-02 20:53:56 +0100

    O

--- code/main.py
+++ code/main.py
@@ -1,11 +1,34 @@
-import ai.simulateur.simulator as smlt
-import ai.simulateur.simconnect_interface as sim
+import simulator as smlt
+import simconnect_interface as sim
+import agent as ag
+import matplotlib.pyplot as plt
 
 def main() :
+    agent = ag.Agent(ag.AI())
+
+    ords = []
+    absc = []
+
+    for h in range (3) :
+        sm = smlt.SimConnect(["thrust","weight","lift","drag"])
+        sim.sim.__init__(sm)
+        for i in range(30) :
+            sm.compute_state(1)
+            agent.compute()
+        print(h)
+
+
     sm = smlt.SimConnect(["thrust","weight","lift","drag"])
     sim.sim.__init__(sm)
+    for i in range(100) :
+        sm.compute_state(1)
+        agent.compute()
 
-    print(sim.call("alt"))
+        if i%3 == 0 :
+            ords.append(sim.call("alt"))
+            absc.append(sim.call("lat"))
+    plt.scatter(absc,ords)
+    plt.show()
 
     return 0
 
